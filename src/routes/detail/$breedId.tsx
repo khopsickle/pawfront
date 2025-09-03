ef46c3f1b4338598c0a9e6fa7a39366626e41849
khopsickle <khopsickle@users.noreply.github.com>
2025-09-03 00:38:02 -0700

    Lazy load BreedDetail

--- src/routes/detail/$breedId.tsx
+++ src/routes/detail/$breedId.tsx
@@ -1,6 +1,20 @@
-import BreedDetail from '@/features/breeds/BreedDetail';
 import { createFileRoute } from '@tanstack/react-router';
+import React, { Suspense } from 'react';
 
+const BreedDetail = React.lazy(
+    () => import('@/features/breeds/BreedDetail'),
+) as React.ComponentType<{ breedId: string }>;
+
+export function BreedDetailWrapper({ breedId }: { breedId: string }) {
+    return (
+        <Suspense fallback={<div>Loading breed details...</div>}>
+            <BreedDetail breedId={breedId} />
+        </Suspense>
+    );
+}
 export const Route = createFileRoute('/detail/$breedId')({
-    component: BreedDetail,
+    component: () => {
+        const { breedId } = Route.useParams();
+        return <BreedDetailWrapper breedId={breedId} />;
+    },
 });
